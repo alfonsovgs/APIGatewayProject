abafe820eca66816689bc9397ea550d98e7ae69b
Alfonso Vargas <vc.alfonso@hotmail.com>
2019-03-24 14:42:42 -0600

    Implementation ApiGateway OCELOT

---
+++ APIGateway/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace APIGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //CreateWebHostBuilder(args).Build().Run();
+            new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                //.UseUrls("https://localhost:9000")
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config
+                        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                        .AddJsonFile("appsettings.json", true, true)
+                        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                        .AddJsonFile("ocelot.json")
+                        .AddEnvironmentVariables();
+                })
+                .ConfigureServices(s => {
+                    s.AddOcelot();
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    //add your logging
+                })
+                .UseIISIntegration()
+                .Configure(app =>
+                {
+                    app.Map("/swagger/v1/swagger.json", b =>
+                    {
+                        b.Run(async x =>
+                        {
+                            var json = File.ReadAllText("swagger.json");
+                            await x.Response.WriteAsync(json);
+                        });
+                    });
+                    app.UseSwaggerUI(c =>
+                    {
+                        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ocelot");
+                    });
+                    app.UseOcelot().Wait();
+                })
+                .UseUrls("https://localhost:9000")
+                .Build()
+                .Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ CustomersAPIServices/Controllers/CustomersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomersAPIServices.Controllers
+{
+    [Route("api/[controller]")]
+    public class CustomersController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] { "Catcher Wong", "James Li" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"Catcher Wong - {id}";
+        }
+    }
+}
